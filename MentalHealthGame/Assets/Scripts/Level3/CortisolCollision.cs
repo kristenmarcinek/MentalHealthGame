5f35831b91e2f28c72ce2749b29c9e58c29ba378
Kristen <kristen.marcinek@rutgers.edu>
2023-10-14 19:02:15 -0400

    edit cortisol

--- MentalHealthGame/Assets/Scripts/Level3/CortisolCollision.cs
+++ MentalHealthGame/Assets/Scripts/Level3/CortisolCollision.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class CortisolCollision : MonoBehaviour
 {
     public Level3Manager level3Man;
+    public bool isUp;
+    //public bool isDown;
+    public float floatSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(this.gameObject.tag == "CortisolUp")
+        {
+            isUp = true;
+        }
+
+        if(this.gameObject.tag == "CortisolDown")
+        {
+            //isDown = true;
+            isUp = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isUp)
+        {
+            transform.position += new Vector3(0, floatSpeed, 0);
+        }
+
+        if(!isUp)
+        {
+            transform.position -= new Vector3(0, floatSpeed, 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -24,6 +44,20 @@ public class CortisolCollision : MonoBehaviour
             level3Man.timeRemaining -= 2f;
 
             Destroy(this.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("Ground") && isUp)
+        {
+            Debug.Log("colliding up");
+            isUp = false;
+            //isDown = true;
+        }
+
+        if(other.gameObject.CompareTag("Ground") && !isUp)
+        {
+            Debug.Log("colliding down");
+            isUp = true;
+            //isDown = false;
         }
     }
 }
