8ba8c307627b4d1c06e86c15e05199b0167fc55f
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-10-15 01:09:21 -0400

    Added scene transition

--- MentalHealthGame/Assets/Scripts/GameManager.cs
+++ MentalHealthGame/Assets/Scripts/GameManager.cs
@@ -2,12 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
-   
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+  
+
+    public void LoadNextLevel()
+    {
+
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Amygdala");
+    }
+
+    public void ShowAboutMenu()
+    {
+        SceneManager.LoadScene("AboutMenu");
+    }
+
+    public void ShowStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
 
    
 
-    
+
+   
+
 }
