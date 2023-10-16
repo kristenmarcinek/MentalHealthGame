51d72889209b0e2ee88c147e549b356b2eb84118
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-10-16 00:54:26 -0400

    Modified Scenes

--- MentalHealthGame/Assets/Scripts/PlayerController.cs
+++ MentalHealthGame/Assets/Scripts/PlayerController.cs
@@ -60,73 +60,50 @@ public class PlayerController : MonoBehaviour
             jumpButtonPressed = false;
         }
 
-        if(isGrounded())
+        // Improved double jump logic
+        if (isGrounded())
         {
-            animator.SetBool("isJumping", false);
+            canDoubleJump = true;
         }
 
-        if(!isGrounded())
+        if (Input.GetButtonDown("Jump"))
         {
-            animator.SetBool("isJumping", true);
+            if (isGrounded() || canDoubleJump)
+            {
+                float jumpHeight = isGrounded() ? jumpForce : doubleJumpForce;
+                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+                canDoubleJump = !isGrounded();
+            }
         }
 
-        float currentJumpForce = jumpForce;
-
-        if (!isGrounded() && jumpButtonPressed)
-        {
-            currentJumpForce = jumpForce;
-        }
-        else
-        {
-            currentJumpForce = jumpForce * runningJump;
-        }
-
-        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f && !isJumping)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, currentJumpForce);
-
-            jumpBufferCounter = 0f;
-
-            StartCoroutine(JumpCooldown());
-        }
-
-        if (isGrounded() && !Input.GetButton("Jump"))
-        {
-            canDoubleJump = false;
-        }
-
-        if (Input.GetButtonDown("Jump") && (isGrounded() || canDoubleJump))
-        {
-            float jumpHeight = isGrounded() ? jumpForce : doubleJumpForce; // Reduce the double jump height here
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-
-            canDoubleJump = !canDoubleJump;
-        }
-
-        Left();
-
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) && isGrounded())
+        // Improved running animation handling
+        if (Mathf.Abs(horizontal) > 0 && isGrounded())
         {
             animator.SetBool("isRunning", true);
         }
-
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) && isGrounded())
+        else
         {
             animator.SetBool("isRunning", false);
         }
+
+        // Simplified jump animation handling
+        animator.SetBool("isJumping", !isGrounded());
+
+        Left();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
+        // Gravity scaling (you can fine-tune the value)
         if (rb.velocity.y < -0.1)
         {
-            ChangeGravity();
+            rb.gravityScale = 2.0f; // Adjust this value as needed
         }
         else
         {
-            rb.gravityScale = 2;
+            rb.gravityScale = 2.0f; // Reset gravity scale to the default value
         }
 
         rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxLength);
@@ -166,3 +143,176 @@ public class PlayerController : MonoBehaviour
         isJumping = false;
     }
 }
+
+
+
+
+
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerController : MonoBehaviour
+//{
+//    private float horizontal;
+//    [SerializeField] float speed = 8f;
+//    [SerializeField] float jumpForce = 16f;
+//    private bool isFacingRight = true;
+//    private bool canDoubleJump;
+//    [SerializeField] int maxLength = 50;
+//    [SerializeField] float runningJump = 0.6f;
+//    [SerializeField] float doubleJumpForce = 12f; // Adjust the double jump height here
+
+//    [SerializeField] Rigidbody2D rb;
+//    [SerializeField] Transform groundCheck;
+//    [SerializeField] LayerMask groundLayer;
+
+//    private bool isJumping;
+//    [SerializeField] float coyoteTime = 0.2f;
+//    private float coyoteTimeCounter;
+//    [SerializeField] float jumpBufferTime = 0.2f;
+//    private float jumpBufferCounter;
+
+//    private bool jumpButtonPressed;
+
+//    [SerializeField] SpriteRenderer sR;
+//    [SerializeField] Animator animator;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        horizontal = Input.GetAxisRaw("Horizontal");
+
+//        if (isGrounded())
+//        {
+//            coyoteTimeCounter = coyoteTime;
+//        }
+//        else
+//        {
+//            coyoteTimeCounter -= Time.deltaTime;
+//        }
+
+//        if (Input.GetButtonDown("Jump"))
+//        {
+//            jumpBufferCounter = jumpBufferTime;
+//            jumpButtonPressed = true;
+//        }
+//        else
+//        {
+//            jumpBufferCounter -= Time.deltaTime;
+//            jumpButtonPressed = false;
+//        }
+
+//        if(isGrounded())
+//        {
+//            animator.SetBool("isJumping", false);
+//        }
+
+//        if(!isGrounded())
+//        {
+//            animator.SetBool("isJumping", true);
+//        }
+
+//        float currentJumpForce = jumpForce;
+
+//        if (!isGrounded() && jumpButtonPressed)
+//        {
+//            currentJumpForce = jumpForce;
+//        }
+//        else
+//        {
+//            currentJumpForce = jumpForce * runningJump;
+//        }
+
+//        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f && !isJumping)
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, currentJumpForce);
+
+//            jumpBufferCounter = 0f;
+
+//            StartCoroutine(JumpCooldown());
+//        }
+
+//        if (isGrounded() && !Input.GetButton("Jump"))
+//        {
+//            canDoubleJump = false;
+//        }
+
+//        if (Input.GetButtonDown("Jump") && (isGrounded() || canDoubleJump))
+//        {
+//            float jumpHeight = isGrounded() ? jumpForce : doubleJumpForce; // Reduce the double jump height here
+//            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+
+//            canDoubleJump = !canDoubleJump;
+//        }
+
+//        Left();
+
+//        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) && isGrounded())
+//        {
+//            animator.SetBool("isRunning", true);
+//        }
+
+//        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) && isGrounded())
+//        {
+//            animator.SetBool("isRunning", false);
+//        }
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+//        if (rb.velocity.y < -0.1)
+//        {
+//            ChangeGravity();
+//        }
+//        else
+//        {
+//            rb.gravityScale = 2;
+//        }
+
+//        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxLength);
+//    }
+
+//    public void Left()
+//    {
+//        if (isFacingRight && horizontal < 0f)
+//        {
+//            isFacingRight = !isFacingRight;
+//            sR.flipX = true;
+//        }
+
+//        if (!isFacingRight && horizontal > 0f)
+//        {
+//            isFacingRight = true;
+//            sR.flipX = false;
+//        }
+
+//        animator.SetFloat("speed", Mathf.Abs(horizontal));
+//    }
+
+//    public bool isGrounded()
+//    {
+//        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+//    }
+
+//    public void ChangeGravity()
+//    {
+//        rb.gravityScale = rb.gravityScale * 1.045f;
+//    }
+
+//    private IEnumerator JumpCooldown()
+//    {
+//        isJumping = true;
+//        yield return new WaitForSeconds(0.4f);
+//        isJumping = false;
+//    }
+//}
